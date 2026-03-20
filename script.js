22efd486215c319a002be94610066d25a635d83f
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-03-20 12:19:37 -0300

    tentando fazer o btn sumir o aside

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let btncompras = document.getElementById("btncompras");
 
 btncompras.addEventListener("click", () => {
+    let div = document.createElement("div")
     let aside = document.createElement("aside");
     let main = document.getElementById("maincompras");
 
-    main.appendChild(aside);
+    main.appendChild(div)
+    div.appendChild(aside);
+    div.id = "InsideRemove";
     aside.classList.add("asidecarrinho");
-    aside.idd = "RemoveId";
+    aside.id = "RemoveId";    
+    document.getElementById("btncompras").id = "btnapagacompras";
+})
+
+let btnapagacompras = document.getElementById("btnapagacompras");
+
+btnapagacompras.addEventListener("click", () => {
+    let div = document.getElementById("InsideRemove");
+    div.remove();
+    document.getElementById("btnapagacompras").id = "btncompras";
 })
