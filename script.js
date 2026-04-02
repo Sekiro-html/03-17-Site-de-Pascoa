e0451d1affab688568680aa20697c500dc395272
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-04-02 09:28:47 -0300

    erro arrumado e tentando fazer o contador funcionar

--- script.js
+++ script.js
@@ -2,47 +2,56 @@ let container = [
     {
         Txt: "Ovo de Páscoa Lacta 170g",
         preco: "R$ 60.00",
-        img: "./IMG/Ovo_Ben_10.webp"
+        img: "./IMG/Ovo_Ben_10.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Páscoa Nocciolate Lindt 350g",
         preco: "R$ 75.00",
-        img: "./IMG/ovo-pascoa-choc-noccio-lindt-cx-350g-1.webp"
+        img: "./IMG/ovo-pascoa-choc-noccio-lindt-cx-350g-1.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Páscoa Chocolate Ao Leite HAVAN",
         preco: "R$ 89.99",
-        img: "./IMG/OVOs HAVAN.webp"
+        img: "./IMG/OVOs HAVAN.webp",
+        quant: 0
     },  
     {
         Txt: "Ovo de Páscoa Scooby-Doo 120g",
         preco: "R$ 42.66",
-        img: "./IMG/Scooby-Loo.webp"
+        img: "./IMG/Scooby-Loo.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Páscoa de Chocolate com copo Enaldinho",
         preco: "R$ 69.99",
-        img: "./IMG/Ovo-do-Enaldinho.webp"
+        img: "./IMG/Ovo-do-Enaldinho.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Páscoa de chocolate Tom e Jerry 80g",
         preco: "R$ 22.90",
-        img: "./IMG/Tom-e-Jerry.webp"
+        img: "./IMG/Tom-e-Jerry.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Pásco de Chocolate Luccas Neto 100g",
         preco: "R$ 126.40",
-        img: "./IMG/Ovo-Do-Lucas.webp"
+        img: "./IMG/Ovo-Do-Lucas.webp",
+        quant: 0
     },
     {
         Txt: "Ovo de Páscoa de Chocolate Toc Mix 50g",
         preco: "R$ 10.99",
-        img: "./IMG/Ovo-Joy.webp"
+        img: "./IMG/Ovo-Joy.webp",
+        quant: 0
     }
 ];
 
 const listadeovos = [];
-const listapreco = []
+const listapreco = [];
+const quantcarrinho = [];
 
 let btncarrinho = document.getElementById("btncarrinho");
 
@@ -51,7 +60,6 @@ btncarrinho.addEventListener("click", () => {
     let body = document.querySelector("#body");
     let btnremove = document.createElement("input");
     let preco = document.createElement("span");
-    let section = document.createElement("section");
     let soma = 0;
 
     body.appendChild(aside);
@@ -59,8 +67,9 @@ btncarrinho.addEventListener("click", () => {
     aside.id = "animacao"
     btnremove.id = "btnexclui"
     btnremove.type = "button";
+    
     for (i = 0; i < listadeovos.length; i++) {
-        const element = listadeovos[i];
+        
         let section = document.createElement("section");
         let img = document.createElement("img");
         let h1 = document.createElement("h1");
@@ -69,32 +78,39 @@ btncarrinho.addEventListener("click", () => {
         let btn1 = document.createElement("input");
         let btn2 = document.createElement("input");
         let ncarac = i + "";
-        console.log(ncarac);
 
         aside.appendChild(section);
         section.id = "sectionovos" + ncarac;
+
         section.appendChild(img);
         section.appendChild(h1);
         section.appendChild(span);
         section.appendChild(btn1);
         section.appendChild(contador);
         section.appendChild(btn2);
-        img.src = container[i].img;
-        h1.innerText = container[i].Txt;
-        span.innerText = container[i].preco;
+
+        img.src = listadeovos[i].img;
+        h1.innerText = listadeovos[i].Txt;
+        span.innerText = listadeovos[i].preco;
         span.id = "precoovo" + ncarac;
         contador.type = "Text";
-        btn1.type = "button"
-        btn2.type = "button"
-
-        
+        contador.value = listadeovos[i].quant;
+        btn1.type = "button";
+        btn2.type = "button";
+        btn1.value = "-";
+        btn2.value = "+";
+        contador.classList.add("Btnscount");
+        btn1.classList.add("Btnscount");
+        btn1.id = "empurabtn"
+        btn2.classList.add("Btnscount");
 
         soma += parseFloat(listapreco[i]);
 
         aside.appendChild(preco);
-        preco.innerText = "R$" + soma
-        preco.id = "precoovos"
-    }
+        preco.innerText = "R$" + soma;
+        preco.id = "precoovos";
+
+    };
 
 
     aside.classList.add("asidecarrinho");
@@ -116,7 +132,7 @@ function CriaContainer(onload) {
         let span = document.createElement("span");
         let btn = document.createElement("input");
         let ncarac = i + "";
-        console.log(ncarac);
+
 
         main.appendChild(section);
         section.id = "sectionovos" + ncarac;
@@ -130,18 +146,23 @@ function CriaContainer(onload) {
         span.id = "precoovo" + ncarac;
         btn.type = "button";
         btn.value = "Comprar";
-        btn.id = "btncompra" + ncarac;   
+        btn.id = "btncompra" + ncarac;
         
 
         let btnovos = document.getElementById("btncompra" + ncarac);
 
-        console.log(btnovos);
+
 
         btnovos.addEventListener("click", () => {
             let guardaovos = container[i];
             let preco = container[i].preco;
+
+            console.log(guardaovos)
             listapreco.push(preco.slice(3));
-            listadeovos.push(guardaovos);   
+            listadeovos.push(guardaovos);
+            let quant = 1;
+            listadeovos[i].quant = quant;
+            quant++;
         });
     }
 }
